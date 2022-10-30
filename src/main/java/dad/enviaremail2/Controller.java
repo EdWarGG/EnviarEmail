1b34db5e7fca22a51c82075aa04b8163a886c6d7
EdWarGG <eduplaypro24@gmail.com>
2022-10-30 12:52:17 +0000

    Initial commit

---
+++ src/main/java/dad/enviaremail2/Controller.java
@@ -0,0 +1,141 @@
+package dad.enviaremail2;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller implements Initializable{
+	
+	//model
+	StringProperty servidor = new SimpleStringProperty();
+	StringProperty puerto = new SimpleStringProperty();
+	StringProperty remitente = new SimpleStringProperty();
+	StringProperty contraseña = new SimpleStringProperty();
+	StringProperty destinatario = new SimpleStringProperty();
+	StringProperty asunto = new SimpleStringProperty();
+	StringProperty mensaje = new SimpleStringProperty();
+	BooleanProperty ssl = new SimpleBooleanProperty();
+	
+	//view
+    @FXML
+    private AnchorPane view;
+    @FXML
+    private TextField asuntoText;
+    @FXML
+    private Button cerrarButton;
+    @FXML
+    private PasswordField contraseñaText;
+    @FXML
+    private TextField destinatarioText;
+    @FXML
+    private Button enviarButton;
+    @FXML
+    private TextArea mensajeText;
+    @FXML
+    private TextField puertoText;
+    @FXML
+    private TextField remitenteText;
+    @FXML
+    private TextField servidorText;
+    @FXML
+    private CheckBox sslCheck;
+    @FXML
+    private Button vaciarButton;
+    
+    
+    public Controller() throws IOException{
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/View.fxml"));
+    	loader.setController(this);
+    	loader.load();
+    }
+    
+    
+	public void initialize(URL location, ResourceBundle resources) {
+		//bindings
+		servidor.bindBidirectional(servidorText.textProperty());
+		puerto.bindBidirectional(puertoText.textProperty());
+		remitente.bindBidirectional(remitenteText.textProperty());
+		contraseña.bindBidirectional(contraseñaText.textProperty());
+		destinatario.bindBidirectional(destinatarioText.textProperty());
+		asunto.bindBidirectional(asuntoText.textProperty());
+		mensaje.bindBidirectional(mensajeText.textProperty());
+		ssl.bindBidirectional(sslCheck.selectedProperty());
+
+	}
+
+	public AnchorPane getView() {
+		return view;
+	}
+
+    @FXML
+    void onCerrarAction(ActionEvent event) {
+    	App.primaryStage.close();
+    }
+
+    @FXML
+    void onEnviarAction(ActionEvent event) throws EmailException {
+		
+		try {
+			Email email = new SimpleEmail();
+			email.setHostName(servidor.getValue());
+			email.setSmtpPort(Integer.parseInt(puerto.getValue()));
+			email.setAuthenticator(new DefaultAuthenticator(remitente.getValue(),contraseña.getValue() ));
+			email.setSSLOnConnect(ssl.getValue());
+			email.setFrom(remitente.getValue());
+			email.setSubject(asunto.getValue());
+			email.setMsg(mensaje.getValue());
+			email.addTo(destinatario.getValue());
+			email.send();
+			Alert alertAcierto = new Alert(AlertType.INFORMATION);
+			alertAcierto.setTitle("Mensaje enviado");
+			alertAcierto.setHeaderText("Mensaje enviado con éxito a '" + destinatario.getValue() + "'.");
+			alertAcierto.showAndWait();
+			
+		} catch (EmailException e) {
+			e.printStackTrace();
+			Alert alertError = new Alert(AlertType.ERROR);
+			alertError.setTitle("Error");
+			alertError.setHeaderText("No se pudo enviar el email.");
+			alertError.setContentText("Invalid message supplied");
+			alertError.showAndWait();
+		}
+
+		
+	}
+    
+
+    @FXML
+    void onVaciarAction(ActionEvent event) {
+    	servidor.setValue("");
+    	puerto.setValue("");
+    	remitente.setValue("");
+    	contraseña.setValue("");
+        destinatario.setValue("");
+    	asunto.setValue("");
+    	mensaje.setValue("");
+    	ssl.setValue(false);
+    }
+
+}
